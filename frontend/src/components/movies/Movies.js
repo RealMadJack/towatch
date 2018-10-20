b2c7deba668d0680efa6c469ad92ac804becd19d
Dmitry G <gupalo1108@gmail.com>
2018-10-20 15:21:46 +0300

    Frontend Movies component card styles

--- frontend/src/components/movies/Movies.js
+++ frontend/src/components/movies/Movies.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 
 import './Movies.sass';
 
+const DefaultThumb = require('../../img/default-thumb.png')
+
 export default class Header extends Component {
   state = {
     movies: [],
@@ -22,16 +24,27 @@ export default class Header extends Component {
 
   render () {
     const movies_list = this.state.movies.map((movie) => {
+      const col_style = {
+        'padding': '0 12px',
+      }
+      const moviegenre_slice = movie.moviegenre.slice(0, 4)
       return (
-        <div className="col-md-3">
+        <div className="col-md-3" style={col_style} key={movie.id}>
           <div className="card">
-            <img className="card-img-top" src="" alt="Card image cap" />
+              <a className="card__moviepanel" href="#">
+                <span>{movie.moviepanel.name}</span>
+              </a>
+              <img className="card-img-top" src={movie.poster_url ? movie.poster_url : DefaultThumb} alt={movie.name} />
             <div className="card-body">
+              <div className="card-body__moviegenres">
+                {moviegenre_slice.map((genre) => <a className="card-body__moviegenres--moviegenre" href="#" key={genre.id}><span>{genre.name}</span></a>)}
+              </div>
               <h5 className="card-title">{movie.name}</h5>
               <p className="card-text">{movie.description}</p>
             </div>
             <div className="card-footer">
               <small className="text-muted">Published at {movie.published_at}</small>
+              {/*<small className="text-muted">Updated at {movie.modified}</small>*/}
             </div>
           </div>
         </div>
