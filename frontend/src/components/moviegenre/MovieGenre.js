1ec6afbca18ed11af0a622e815890501251e64f6
Dmitry G <gupalo1108@gmail.com>
2018-10-22 14:08:12 +0300

    Frontend: added movie-list to MovieGenre component

--- frontend/src/components/moviegenre/MovieGenre.js
+++ frontend/src/components/moviegenre/MovieGenre.js
@@ -82,11 +82,57 @@ export default class MovieGenre extends Component {
       )
     }
 
+    const col_style = {
+      'padding': '0 12px',
+    };
+    const moviegenre = this.state.moviegenre
+    const moviegenre_movies = moviegenre.movies.map((movie) => {
+      const moviepanel_url = `/${movie.moviepanel.slug}/`;
+      const movie_url = `/${movie.moviepanel.slug}/${movie.id}/`;
+      const moviegenre_slice = movie.moviegenre.slice(0, 4);
+      const moviegenres = moviegenre_slice.map((moviegenre) => {
+        const moviegenre_url = `/${moviegenre.moviepanel.slug}/${moviegenre.slug}/`;
+        return (
+          <a className="card-body__moviegenres--moviegenre" href={moviegenre_url} key={moviegenre.id}>
+            <span>{moviegenre.name}</span>
+          </a>
+        );
+      });
+
+      return(
+        <div className="col-md-3" style={col_style} key={movie.id}>
+          <div className="card" href={movie_url}> {/* Todo card <a> */}
+            <a className="card__moviepanel" href={moviepanel_url}>
+              <span>{movie.moviepanel.name}</span>
+            </a>
+            <img className="card-img-top" src={movie.poster_url ? movie.poster_url : DefaultThumb} alt={movie.name} />
+            <div className="card-body">
+              <div className="card-body__moviegenres">
+                {moviegenres}
+              </div>
+              <h5 className="card-title">{movie.name}</h5>
+              <p className="card-text">{movie.description}</p>
+            </div>
+            <div className="card-footer">
+              <small className="text-muted float-left">Published at: </small>
+              <small className="text-muted float-right">
+                {Moment(movie.published_at).format('d MMM HH:mm')}
+              </small>
+              {/*<small className="text-muted">Updated at {movie.modified}</small>*/}
+            </div>
+          </div>
+        </div>
+      );
+    })
 
     if (this.state.error.status === 200) {
       return(
-        <div>
-          genre is working
+        <div className="container">
+          <div className="row">
+            <div className="card-deck">
+              {moviegenre_movies}
+            </div>
+          </div>
         </div>
       );
     } else {
