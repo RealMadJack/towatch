864e4d39ce5db6efe7d997a7a253e7f46df7d2e9
Dmitry G <gupalo1108@gmail.com>
2018-11-02 15:22:20 +0300

    Backend: IMBD scraper initial algorithm, OOP

--- towatch/apps/moviepanel/scrapers.py
+++ towatch/apps/moviepanel/scrapers.py
@@ -9,16 +9,43 @@ import threading
 
 from datetime import datetime
 
+from .models import Movie
+
 
 class IMDBScraper:
     """
     IMDB search-scrap movie rank and general info
+    query movies => filter movies without info => create queue => add movies to queue
+    => search imbd movie by the title => if found => visit first result uri => scrap data
+                                      => if not => log result (in prod db table)
+    => validate scraped data => save Movie object
     """
 
-    def __init__(self):
+    def __init__(self, target_url='imdb.com'):
         logging.info(f'Creating instance: {self.__class__.__name__}')
+        self.target_url = target_url
+
+    def get_filter_db_movies(self):
+        """
+        if movie has imdb True, don't search
+        """
+        pass
+
+    def create_movie_queue(self):
+        pass
+
+    def search_imdb_movie(self):
+        pass
 
     def run(self):
+        """
+        Queue, threading cycle
+        """
+        filtered_movies = self.get_db_movies()
+        print(filtered_movies)
+
+        # cycle trough que and search movie
+
         logging.info(f'Starting {self.__class__.__name__}')
 
 
